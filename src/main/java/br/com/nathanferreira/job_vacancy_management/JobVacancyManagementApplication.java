ea16db24311d3a2002e8c85df69404f11d8a197e
Nathan <nathanferreira00@hotmail.com>
2024-02-09 08:57:26 -0300

    first commit

---
+++ src/main/java/br/com/nathanferreira/job_vacancy_management/JobVacancyManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.nathanferreira.job_vacancy_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobVacancyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobVacancyManagementApplication.class, args);
+	}
+
+}
