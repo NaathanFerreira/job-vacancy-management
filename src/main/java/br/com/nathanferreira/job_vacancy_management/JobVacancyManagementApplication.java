b08b3acdf65322889f34feea3015e79693d458b3
Nathan <nathanferreira00@hotmail.com>
2024-02-15 14:31:19 -0300

    feat: swagger config

--- src/main/java/br/com/nathanferreira/job_vacancy_management/JobVacancyManagementApplication.java
+++ src/main/java/br/com/nathanferreira/job_vacancy_management/JobVacancyManagementApplication.java
@@ -3,7 +3,17 @@ package br.com.nathanferreira.job_vacancy_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition (
+	info = @Info(
+		title = "Job Vacancy Management",
+		description = "API responsable for manage job vacancies",
+		version = "1"
+	)
+)
 public class JobVacancyManagementApplication {
 
 	public static void main(String[] args) {
