d3e9652ff9c62b8de72906b7eed5d84f327a6973
Nathan <nathanferreira00@hotmail.com>
2024-02-16 15:06:45 -0300

    docs: create jobs docs

--- src/main/java/br/com/nathanferreira/job_vacancy_management/modules/company/controllers/JobController.java
+++ src/main/java/br/com/nathanferreira/job_vacancy_management/modules/company/controllers/JobController.java
@@ -6,6 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.nathanferreira.job_vacancy_management.modules.company.dto.CreateJobDTO;
 import br.com.nathanferreira.job_vacancy_management.modules.company.entities.JobEntity;
 import br.com.nathanferreira.job_vacancy_management.modules.company.useCases.CreateJobUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
@@ -26,6 +34,14 @@ public class JobController {
 
   @PostMapping("/")
   @PreAuthorize("hasRole('COMPANY')")
+  @Tag(name = "Vacancies", description = "Vacancy infos")
+  @Operation(summary = "Vacancy registration", description = "Endpoint responsible for creating a new vacancy")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", content = {
+          @Content(schema = @Schema(implementation = JobEntity.class))
+      })
+  })
+  @SecurityRequirement(name = "jwt_auth")
   public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO jobDTO, HttpServletRequest request) {
 
     // attribute defined in SecurityFilter
--- src/main/java/br/com/nathanferreira/job_vacancy_management/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/nathanferreira/job_vacancy_management/modules/company/dto/CreateJobDTO.java
@@ -1,15 +1,21 @@
 package br.com.nathanferreira.job_vacancy_management.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class CreateJobDTO {
   
+  @Schema(example = "Javascript Jr Developer")
   private String description;
 
+  @Schema(example = "GymPass", requiredMode = RequiredMode.REQUIRED)
   @NotBlank(message = "Field (benefits) is required")
   private String benefits;
+
+  @Schema(example = "Junior", requiredMode = RequiredMode.REQUIRED)
   private String level;
 
 }
